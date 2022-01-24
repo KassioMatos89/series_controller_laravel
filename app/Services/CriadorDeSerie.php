9e953494459e592987631c135c79db55cbe38d1d
Kassio Matos <kassio_29@hotmail.com>
2022-01-24 09:09:42 -0300

    chore: Initial commit

---
+++ app/Services/CriadorDeSerie.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\Serie;
+use Illuminate\Support\Facades\DB;
+
+class CriadorDeSerie
+{
+    public function criarSerie(
+        string $nomeSerie,
+        int $qtdTemporadas,
+        int $epPorTemporada
+    ): Serie
+    {
+        // BeginTransaction Laravel
+        DB::beginTransaction();
+        $serie = Serie::create(['nome' => $nomeSerie]);
+        $this->criarTemporadas($qtdTemporadas, $epPorTemporada, $serie);
+        DB::commit();
+
+        return $serie;
+    }
+
+    /**
+     * Cria as temporadas de uma série
+     *
+     * @param integer $qtdTemporadas
+     * @param integer $epPorTemporada
+     * @param Serie $serie
+     * @return void
+     */
+    private function criarTemporadas(int $qtdTemporadas, int $epPorTemporada, Serie $serie): void
+    {
+        for ($i = 1; $i <= $qtdTemporadas; $i++) {
+            $temporada = $serie->temporadas()->create(['numero' => $i]);
+
+            $this->criarEpisodios($epPorTemporada, $temporada);
+        }
+    }
+
+    /**
+     * Cria episodios de uma temporada
+     *
+     * @param integer $epPorTemporada
+     * @param \Illuminate\Database\Eloquent\Model $temporada
+     * @return void
+     */
+    private function criarEpisodios(int $epPorTemporada, \Illuminate\Database\Eloquent\Model $temporada): void
+    {
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
+    }
+}
---
+++ app/Services/RemovedorDeSerie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use App\{Serie, Temporada, Episodio};
+
+class RemovedorDeSerie
+{
+    public function removerSerie(int $serieId): string
+    {
+        $nomeSerie = '';
+        DB::transaction(function () use ($serieId, &$nomeSerie) {
+            $serie = Serie::find($serieId);
+            $nomeSerie = $serie->nome;
+
+            $this->removerTemporadas($serie);
+            $serie->delete();
+        });
+
+        return $nomeSerie;
+    }
+
+    /**
+     * Remove Serie and its Temporadas
+     *
+     * @param Serie $serie
+     * @return void
+     */
+    private function removerTemporadas(Serie $serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+            $this->removerEpisodios($temporada);
+            $temporada->delete();
+        });
+    }
+
+    /**
+     * Remove Temporada
+     *
+     * @param Temporada $temporada
+     * @return void
+     */
+    private function removerEpisodios(Temporada $temporada): void
+    {
+        $temporada->episodios->each(function (Episodio $episodio) {
+            $episodio->delete();
+        });
+    }
+}
